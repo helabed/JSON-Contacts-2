12c963c25219e691706803732e6de674b75e380b
helabed <hani.elabed@gettyimages.com>
2016-03-23 21:18:27 -0500

    Initial Commit

---
+++ JSON Contacts 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Contacts 2
+//
+//  Created by Hani Elabed on 3/23/16.
+//  Copyright © 2016 Hani Elabed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
