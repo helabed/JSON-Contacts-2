19224096e985cb36485d09d7a974409cb8363eb3
helabed <hani.elabed@gettyimages.com>
2016-03-27 11:42:24 -0500

    Added UI to Add New Contact view controller and
    Done button action

---
+++ JSON Contacts 2/AddNewContactViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddNewContactViewController.swift
+//  JSON Contacts 2
+//
+//  Created by Hani Elabed on 3/27/16.
+//  Copyright © 2016 Hani Elabed. All rights reserved.
+//
+
+import UIKit
+
+class AddNewContactViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cellPhoneTextField: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func doneButtonTapped(sender: UIBarButtonItem) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
